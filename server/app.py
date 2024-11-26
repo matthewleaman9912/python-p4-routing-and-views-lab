9b00061753381cf1632589983577ccd781ecd480
MatthewLeaman <leaman.30@osu.edu>
2024-11-26 00:16:56 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -6,3 +6,38 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:string>')
+def print_string(string):
+    print(string)
+    return f'{string}'
+
+@app.route('/count/<int:number>')
+def count(number):
+    numbers = list(range(0, number, 1))
+    return '\n'.join([str(num) for num in numbers])+'\n'
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '*':
+        number = num1 * num2
+        return str(number)
+    elif operation == '+':
+        number = num1 + num2
+        return str(number)
+    elif operation == '-':
+        number = num1 - num2
+        return str(number)
+    elif operation == 'div':
+        number = (num1 / num2)
+        return str(number)
+    elif operation == '%':
+        number = num1 % num2
+        return str(number)
+    else:
+        return None
+    
